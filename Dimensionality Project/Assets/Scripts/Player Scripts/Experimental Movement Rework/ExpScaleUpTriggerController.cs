cf55a56b1c76f39be1af448bf6dad50cfb059873
William <william.jcs@gmail.com>
2022-05-18 00:53:28 +0100

    Start work on experimental movement controller rework

---
+++ Dimensionality Project/Assets/Scripts/Player Scripts/Experimental Movement Rework/ExpScaleUpTriggerController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class ExpScaleUpTriggerController : MonoBehaviour
+{
+    public ExpPlayerScalingController scalingController;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Ground") || other.gameObject.layer == LayerMask.NameToLayer("Wall"))
+        {
+            scalingController.IsRoomToScaleUp = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Ground") || other.gameObject.layer == LayerMask.NameToLayer("Wall"))
+        {
+            scalingController.IsRoomToScaleUp = true;
+        }
+    }
+}
