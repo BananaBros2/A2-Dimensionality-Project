0f86f35416c0d5874ce5cb1c15b9c5f7cfdeeae5
Ethan <19109@totton.ac.uk>
2022-05-18 14:59:10 +0100

    Finished with bug
    , +

--- Dimensionality Project/Assets/Scripts/TeleportController.cs
+++ Dimensionality Project/Assets/Scripts/TeleportController.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class TeleportController : MonoBehaviour
 {
     //private float cooldown = 10f;
+    CapsuleCollider capsuleCollider;
 
     public float range = 5f;
+    public float coolDown = 1f;
+
+    private float cooldowntime;
+    private float time;
+
+    public PlayerController playerController;
 
     public Vector3 targetPosition;
+    Vector3 topOfCap;
+    Vector3 botOfCap;
 
     public GameObject target;
     public GameObject player;
@@ -17,9 +26,16 @@ public class TeleportController : MonoBehaviour
 
     public LayerMask GroundMask;
 
+    private void Start()
+    {
+        capsuleCollider = GetComponentInChildren<CapsuleCollider>();
+    }
+
     // Update is called once per frame
     void Update()
     {
+        time += Time.deltaTime;
+
         if (Input.GetButtonDown("Teleport"))
         {
 
@@ -27,31 +43,43 @@ public class TeleportController : MonoBehaviour
 
             targetPosition = transform.position + or.forward * range;
 
-            Teleport(); //calls function
+            topOfCap = new Vector3(player.transform.position.x, player.transform.position.y + capsuleCollider.height / 2f, player.transform.position.z);
+            botOfCap = new Vector3(player.transform.position.x, player.transform.position.y - capsuleCollider.height / 2f, player.transform.position.z);
+
+            if (time >= cooldowntime)
+            {
+                Teleport(); //calls function
+
+            }
+            else
+            {
+                print("on cool down");
+            }
         }
 
         
     }
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(topOfCap, 0.5f);
+        Gizmos.DrawSphere(botOfCap, 0.5f);
+    }
+
     void Teleport()
     {
         RaycastHit hit;
-        Rigidbody rb = GetComponent<Rigidbody>();
-        CapsuleCollider capsuleCollider = GetComponentInChildren<CapsuleCollider>();
-        //Vector3 topPoint = Vector3.up * transform.position.y / 2f;
-        //Vector3 bottomPoint = transform.position - Vector3.down * 2;
 
-        //// Cast character controller shape 10 meters forward to see if it is about to hit anything.
-        //if (Physics.CapsuleCast(topPoint, bottomPoint, capsuleCollider.radius,  transform.forward, out hit, 10))
-        //player.transform.position = hit.transform.position; // hit.distance;
-
-        if (Physics.CapsuleCast(transform.position, targetPosition, rb.transform.localScale.x, or.forward, out hit, 10000))
+        cooldowntime = time + coolDown;
+        
+        if (Physics.CapsuleCast(topOfCap, botOfCap, capsuleCollider.radius * playerController.PlayerHeight / 2f, or.forward, out hit, range))
         {
-            print(hit.transform.name);
+            player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, hit.point.z);
         }
         else
         {
             player.transform.position = targetPosition;
         }
+        
     }
 }
