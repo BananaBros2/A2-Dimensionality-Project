6f2e8f8f3386c2560d37942968a1376e7b041a72
William J. C. Shepherd <18550@totton.ac.uk>
2022-05-18 13:58:45 +0100

    Finish camera controller and implement movement intent

--- Dimensionality Project/Assets/Scripts/Player Scripts/Experimental Movement Rework/ExpPlayerMovementController.cs
+++ Dimensionality Project/Assets/Scripts/Player Scripts/Experimental Movement Rework/ExpPlayerMovementController.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class ExpPlayerMovementController : MonoBehaviour
 {
+    public ExpPlayerStateController stateController;
+    public Transform facingDirection;
+    public float maxSpeed;
+    public float jumpForce;
+
+    private Vector3 movementIntent = Vector3.zero;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,50 @@ public class ExpPlayerMovementController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateMovementIntent();
+    }
+
+    private void UpdateMovementIntent()
+    {
+        switch (Input.GetAxisRaw("Forward/Backward Movement"))
+        {
+            case 1f: // forward
+                movementIntent = Vector3.forward;
+                break;
+
+            case -1f: // backwards
+                movementIntent = Vector3.back;
+                break;
+
+            case 0f: // no/both input(s)
+                movementIntent = Vector3.zero;
+                break;
+        }
+        switch (Input.GetAxisRaw("Strafe"))
+        {
+            case 1f: // right
+                movementIntent += Vector3.right;
+                break;
+
+            case -1f: // left
+                movementIntent += Vector3.left;
+                break;
+
+            case 0f: // no/both input(s)
+                movementIntent += Vector3.zero;
+                break;
+        }
+        movementIntent = movementIntent.normalized; // may or may not keep this line, depending on simplest implementation of UpdateMovement
+        Debug.Log(movementIntent.ToString());
+    }
+
+    private void FixedUpdate()
+    {
+        UpdateMovement();
+    }
+
+    private void UpdateMovement()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
     }
 }
