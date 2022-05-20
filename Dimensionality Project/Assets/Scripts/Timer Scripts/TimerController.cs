b3c30fc667619cb7c34d95c52e50791c8bdd2772
Dominic <18015@totton.ac.uk>
2022-05-20 10:32:19 +0100

    scaling controls re-implemented

--- Dimensionality Project/Assets/Scripts/Timer Scripts/TimerController.cs
+++ Dimensionality Project/Assets/Scripts/Timer Scripts/TimerController.cs
@@ -38,7 +38,6 @@ public class TimerController : MonoBehaviour
         
         if (Input.GetKeyDown(KeyCode.R))
         {
-            Debug.Log("Better use build not editor"); // don't use my computer please but use the builds on teams or github.com
             SceneManager.LoadScene(2, LoadSceneMode.Single);
         }
 
