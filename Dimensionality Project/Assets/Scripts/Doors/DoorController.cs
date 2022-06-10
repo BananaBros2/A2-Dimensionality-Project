f09e3d6b3fb8e8e2bbbf50d4d52423835c79ad25
Ethan <19109@totton.ac.uk>
2022-06-10 14:04:53 +0100

    commented doorController code

--- Dimensionality Project/Assets/Scripts/Doors/DoorController.cs
+++ Dimensionality Project/Assets/Scripts/Doors/DoorController.cs
@@ -22,59 +22,64 @@ public class DoorController : MonoBehaviour
 
     private void Start()
     {
-        closedRight = doorRight.transform.position;
-        closedLeft = doorLeft.transform.position;
-        
+        closedRight = doorRight.transform.position; //gets the position of the door when it is closed
+        closedLeft = doorLeft.transform.position; //gets the position of the door when it is closed.
+
     }
 
     private void Update()
     {
         if (toOpen)
         {
-            OpenDoor();
+            OpenDoor(); //if toOpen bool value changes to true, calls OpenDoor function.
         }
         else if (toClose)
         {
-            CloseDoor();
+            CloseDoor(); //if toClose bool value changes to true, calls CloseDoor function.
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other) // calls if an object enters the collider.
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player") //checks if the thing that collided is the player.
         {
-            toClose = false;
-            toOpen = true;
+            toClose = false; //changes toClose bool value to false
+            toOpen = true; //changes toOpen bool value to true
         }
     }
 
-    private void OnTriggerExit(Collider other)
+    private void OnTriggerExit(Collider other) //calls if an object exits the door collider.
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player") //checks if the object exiting the collider is the player.
         {
-            toClose = true;
-            toOpen = false;
+            toClose = true; //changes toClose bool value to true
+            toOpen = false; //changes toOpen bool value to false
         }
     }
 
-    private void OpenDoor()
+    private void OpenDoor() //OpenDoor function
     {
         doorRight.transform.position = Vector3.MoveTowards(doorRight.transform.position, targetRight.position, speed * Time.deltaTime);
+        //Moves the Right door towards the target position using a set speed relative to the FPS
         doorLeft.transform.position = Vector3.MoveTowards(doorLeft.transform.position, targetLeft.position, speed * Time.deltaTime);
+        //Moves the Left door towards the target position using a set speed relative to the FPS
         if (doorRight.transform.position == targetRight.position)
+
         {
-            //doorState = DoorState.Open;
+            doorState = DoorState.Open;
         }
         else
         {
             doorState = DoorState.Opening;
-        }
+        }//These lines of code made it easy for us to see where the script got up to when using debug.log
     }
 
-    private void CloseDoor()
+    private void CloseDoor() //CloseDoor function
     {
         doorRight.transform.position = Vector3.MoveTowards(doorRight.transform.position, closedRight, speed * Time.deltaTime);
+        //Moves the Right door towards the close position using a set speed relative to the FPS
         doorLeft.transform.position = Vector3.MoveTowards(doorLeft.transform.position, closedLeft, speed * Time.deltaTime);
+        //Moves the Left door towards the close position using a set speed relative to the FPS
         if (doorRight.transform.position == closedRight)
         {
             doorState = DoorState.Closed;
@@ -82,7 +87,7 @@ public class DoorController : MonoBehaviour
         else
         {
             doorState = DoorState.Closing;
-        }
+        }//These lines of code made it easy for us to see where the script got up to when using debug.log
     }
 
     private enum DoorState
@@ -91,6 +96,6 @@ public class DoorController : MonoBehaviour
         Opening,
         Open,
         Closing
-    }
+    }//These lines of code made it easy for us to see where the script got up to when using debug.log
 
 }
