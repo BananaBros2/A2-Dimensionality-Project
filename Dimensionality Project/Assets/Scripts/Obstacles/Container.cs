32c8c803011b75ec87db752e4cb034792393da3d
Vince <18029@totton.ac.uk>
2022-06-24 00:52:09 +0100

    Organisation and start of Tutorial Remake

---
+++ Dimensionality Project/Assets/Scripts/Obstacles/Container.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Container : MonoBehaviour
+{
+    [SerializeField] public List<GameObject> buttons;
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i <= buttons.Count - 1; i++)
+        {
+            //print(buttons[i]);
+            if (buttons[i].GetComponent<PowerButton>().Pressed == false)
+            {
+                return;
+            }
+            if (i == buttons.Count - 1)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
